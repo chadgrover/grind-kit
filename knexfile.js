a55c5ca2323acbfcc701485408c0583978be54af
chadgrover <93070367+chadgrover@users.noreply.github.com>
2022-11-11 12:48:39 +0900

    Feat: Implement Knex.js settings

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+module.exports = {
+    client: 'pg',
+    connection: process.env.DB_URL_LOCAL,
+    searchPath: 'public',
+    pool: {
+        min: 2,
+        max: 10
+    },
+    migrations: {
+        tableName: 'knex_migrations',
+        directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+        directory: __dirname + '/db/seeds'
+    }
+}
