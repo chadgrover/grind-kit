e1e1679dcdfd4b222af16b7f8b266fb0f7c5887f
chadgrover <93070367+chadgrover@users.noreply.github.com>
2022-11-12 19:03:52 +0900

    Fix: Add NODE_ENV variable

--- db/knex.js
+++ db/knex.js
@@ -2,5 +2,5 @@ const knex = require("knex");
 const config = require("../knexfile");
 
 module.exports = knex(
-  process.env.PORT ? config.production : config.development
+  process.env.NODE_ENV === "production" ? config.production : config.development
 );
