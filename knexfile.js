89c74c2ec89e7a6b6dd5f71c8aec6c786ef64128
chadgrover <93070367+chadgrover@users.noreply.github.com>
2022-11-12 12:21:39 +0900

    Fix: Connect postgres db

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,13 @@
-require("dotenv").config();
+require('dotenv').config();
 
 module.exports = {
   development: {
     client: "pg",
-    connection: process.env.DB_URL_LOCAL,
+    connection: {
+      host: process.env.host,
+      user: process.env.user,
+      database: process.env.database,
+    },
     searchPath: "public",
     pool: {
       min: 2,
--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const path = require("path");
+const db = require("../db/knex");
+const bodyParser = require("body-parser");
 
 function setupServer() {
   const app = express();
@@ -6,9 +9,21 @@ function setupServer() {
   // middleware
 
   app.use(express.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(express.static(path.resolve(__dirname, "../client/build")));
 
   app.get("/test", (req, res) => {
     res.status(200).send("Hello 🌏");
+  });
+
+  app.get("/api/instancecontent", async (req, res) => {
+    try {
+      const instancecontent = await db("twg")
+        .select("*")
+        .timeout(1500);
+    } catch (error) {
+      console.error(error);
+    }
   });
 
   return app;
