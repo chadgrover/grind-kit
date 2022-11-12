002ab986f8399a4d6af88dda1434e752a1fa3762
chadgrover <93070367+chadgrover@users.noreply.github.com>
2022-11-12 16:57:16 +0900

    Feat: Implement instancecontent server endpoint

--- server/server.js
+++ server/server.js
@@ -6,23 +6,19 @@ const bodyParser = require("body-parser");
 function setupServer() {
   const app = express();
 
-  // middleware
-
   app.use(express.json());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(express.static(path.resolve(__dirname, "../client/build")));
 
-  app.get("/test", (req, res) => {
-    res.status(200).send("Hello 🌏");
-  });
-
-  app.get("/api/instancecontent", async (req, res) => {
+  app.get("/instancecontent", async (req, res) => {
     try {
-      const instancecontent = await db("twg")
+      const data = await db("twg")
         .select("*")
+        .from("instancecontent_table")
         .timeout(1500);
+      res.status(200).send(data);
     } catch (error) {
-      console.error(error);
+      res.status(500).send(error);
     }
   });
 
