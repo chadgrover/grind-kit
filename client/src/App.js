f10b6bf8631cb402d97a36db4277cbf6efcae1da
chadgrover <93070367+chadgrover@users.noreply.github.com>
2022-11-13 12:22:36 +0900

    Feat: Implement Login and Register components

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,33 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login"
 
 function App() {
+  // Below is code for returning the list of dungeon names and required levels
 
-  const [instanceContent, setInstanceContent] = useState([]);
+  ////import { useState, useEffect } from "react";
+  //import axios from 'axios';
+  // const [instanceContent, setInstanceContent] = useState([]);
 
-  useEffect(() => {
-    getInstanceContent();
-  }, [])
+  // useEffect(() => {
+  //   getInstanceContent();
+  // }, [])
 
-  const getInstanceContent = async () => {
-    const arrayOfInstanceContent = await axios.get('/api/instancecontent');
-    const arrayOfFilteredInstanceContent = arrayOfInstanceContent.data.filter(dungeon => dungeon.name && dungeon.required_level);
-    const arrayOfInstanceContentNames = arrayOfFilteredInstanceContent.map ((dungeon) => {
-      return <p>{dungeon.name} Level: {dungeon.required_level}</p>;
-    });
-    setInstanceContent(arrayOfInstanceContentNames);
-  }
+  // const getInstanceContent = async () => {
+  //   const arrayOfInstanceContent = await axios.get('/api/instancecontent');
+  //   const arrayOfFilteredInstanceContent = arrayOfInstanceContent.data.filter(dungeon => dungeon.name && dungeon.required_level);
+  //   const arrayOfInstanceContentNames = arrayOfFilteredInstanceContent.map ((dungeon) => {
+  //     return <p>{dungeon.name} Level: {dungeon.required_level}</p>;
+  //   });
+  //   setInstanceContent(arrayOfInstanceContentNames);
+  // }
 
   return (
-    <div className="App">
-      <h1>FFXIV Dungeons</h1>
-      {instanceContent}
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+      </Switch>
+    </Router>
   );
 }
 
