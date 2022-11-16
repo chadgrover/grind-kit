76a6af3d6654d8dca285a3b41f6f682598d1cae4
chadgrover <93070367+chadgrover@users.noreply.github.com>
2022-11-16 10:54:24 +0900

    Feat: Implement handleBannerFetch

--- client/src/components/AllDungeons.js
+++ client/src/components/AllDungeons.js
@@ -1,12 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
+import axios from "axios";
 
 function AllDungeons(props) {
   const { instanceContentData, currentLevel } = props;
+  const [bannersArray, setBannersArray] = useState([]);
 
   const filteredInstanceContent = instanceContentData
     .filter(
@@ -15,11 +17,24 @@ function AllDungeons(props) {
         instanceContent.required_level !== null
     )
     .sort((a, b) => b.required_level - a.required_level)
-    .slice(0, 7);
+    .slice(0, 3);
+
+  const handleBannerFetch = async () => {
+    for (const instanceContent of filteredInstanceContent) {
+      const extendedInformation = await axios.get(
+        `https://xivapi.com${instanceContent["url"]}`
+      );
+      setBannersArray(bannersArray => [...bannersArray, extendedInformation.data["Banner"]]);
+    }
+  };
 
   useEffect(() => {
-    console.log(filteredInstanceContent);
-  }, [filteredInstanceContent]);
+    handleBannerFetch();
+  }, []);
+
+  useEffect(() => {
+    console.log(bannersArray);
+  }, [bannersArray]);
 
   return (
     <div>
@@ -27,18 +42,16 @@ function AllDungeons(props) {
         return (
           <Card sx={{ maxWidth: 345 }}>
             <CardActionArea>
-              <CardMedia
-                component="img"
-                height="140"
-                image="/static/images/cards/contemplative-reptile.jpg"
-                alt="green iguana"
-              />
+              <CardMedia component="img" height="140" image="" alt="Banner" />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
                   {instanceContent.name}
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                  EXP: {instanceContent.clear_exp}
+                  Required Level: {instanceContent.required_level}
+                </Typography>
+                <Typography variant="body3" color="text.secondary">
+                  {instanceContent.clear_exp} EXP upon completion
                 </Typography>
               </CardContent>
             </CardActionArea>
