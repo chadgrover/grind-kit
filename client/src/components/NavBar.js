9f84d0cda6f783bf6610e45a8f76d11c77b7ddf0
chadgrover <93070367+chadgrover@users.noreply.github.com>
2022-11-16 12:08:53 +0900

    Feat: Implement NavBar styling

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,19 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { logout } from "../firebase";
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import "../styles/NavBar.css";
 
 function NavBar({ isOpen }) {
   return (
     <>
       {isOpen === true ? (
-        <div>
-          <nav>
-            <Link to="/dashboard">Dashboard</Link>
-            <Link to="/levelinput">Input</Link>
-            <Link to="/session">Session</Link>
-            <Link onClick={logout}>Logout</Link>
-          </nav>
-        </div>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h4">Grind Kit</Typography>
+            <div>
+              <Link to="/dashboard">Dashboard</Link>
+              <Link to="/levelinput">Input</Link>
+              <Link to="/session">Session</Link>
+              <Link onClick={logout}>Logout</Link>
+            </div>
+          </Toolbar>
+        </AppBar>
       ) : (
         <div>
           <p>Log-in Page</p>
